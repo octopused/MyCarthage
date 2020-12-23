548ce0e2a24a63e89286b1a438c89a41d4dfe041
octopused <octopused@gmail.com>
2020-12-23 11:12:17 +0500

    added sample classes

---
+++ MyCarthage/MyCarthageClass.swift
@@ -0,0 +1,15 @@
+//
+//  MyCarthageClass.swift
+//  MyCarthage
+//
+//  Created by r.mingazov on 23.12.2020.
+//
+
+import Foundation
+
+open class MyCarthageClass: MyCarthageProtocol
+{
+    public func some() {
+        
+    }
+}
---
+++ MyCarthage/MyCarthageProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MyCarthageProtocol.swift
+//  MyCarthage
+//
+//  Created by r.mingazov on 22.12.2020.
+//
+
+import Foundation
+
+public protocol MyCarthageProtocol
+{
+    func some()
+}
