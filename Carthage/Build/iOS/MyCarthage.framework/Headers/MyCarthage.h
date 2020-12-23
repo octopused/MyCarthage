fac5cb6d5e3dc5788b46a6e1fbc3ec7203b5ff6a
octopused <octopused@gmail.com>
2020-12-23 11:47:00 +0500

    carthage build

---
+++ Carthage/Build/iOS/MyCarthage.framework/Headers/MyCarthage.h
@@ -0,0 +1,18 @@
+//
+//  MyCarthage.h
+//  MyCarthage
+//
+//  Created by r.mingazov on 22.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyCarthage.
+FOUNDATION_EXPORT double MyCarthageVersionNumber;
+
+//! Project version string for MyCarthage.
+FOUNDATION_EXPORT const unsigned char MyCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCarthage/PublicHeader.h>
+
+
